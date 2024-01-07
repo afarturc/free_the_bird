e838edf3c9d42e58275cb7daaf82ca46ad62b9bd
Artur Correia <art.afo@outlook.pt>
2024-01-07 18:50:15 +0000

    Bug fix: Edit profile button in other users

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -4,24 +4,8 @@ class Chirp < ApplicationRecord
 
   validates :body, presence: true
 
-  belongs_to :parent_chirp, inverse_of: :reply_chirps, foreign_key: :parent_chirp_id, class_name: "Chirp", optional: true
+  belongs_to :parent_chirp, inverse_of: :reply_chirps, foreign_key: :parent_chirp_id, class_name: "Chirp", optional: true, counter_cache: :replies_count
   has_many :reply_chirps, foreign_key: :parent_chirp_id, class_name: "Chirp"
 
   scope :search, -> (field_name, search_string) { where("#{field_name} LIKE ?", "%#{search_string}%") }
-
-
-  after_create :increment_if_reply
-  after_destroy :decrement_if_reply
-
-  private
-
-  def increment_if_reply
-    parent_chirp = self.parent_chirp
-    parent_chirp.update(replies_count: parent_chirp.replies_count + 1) unless self.parent_chirp.nil?
-  end
-
-  def decrement_if_reply
-    parent_chirp = self.parent_chirp
-    parent_chirp.update(replies_count: parent_chirp.replies_count - 1) unless self.parent_chirp.nil?
-  end
 end
