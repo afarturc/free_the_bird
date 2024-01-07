aeabb60cc5cca1bed3b7b8d46ad5bf7cff380a49
Artur Correia <art.afo@outlook.pt>
2024-01-07 15:36:36 +0000

    Updatd navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @chirps = Chirp.where(user: @user)
+    @chirps = Chirp.where(user: @user).order(created_at: :desc)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def selected_link(link_name)
     controller_name == link_name ? "text-black" : "text-gray-400"
   end
+
+  def user_link(link_name)
+    user_id = params[:id].to_i
+    controller_name == link_name && current_user.id == user_id ? "text-black" : "text-gray-400"
+  end
 end
